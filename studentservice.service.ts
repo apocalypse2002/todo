0fcb4fcad62e6bdfee47778f0dd84e99a578d6e5
apocalypse2002 <80613341+apocalypse2002@users.noreply.github.com>
2021-11-15 16:58:47 +0530

    Update studentservice.service.ts

--- studentservice.service.ts
+++ studentservice.service.ts
@@ -1,22 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { TodoDetailsComponent } from './todo-details/todo-details.component';
-import { HttpClientModule } from '@angular/common/http';
-@NgModule({
-  declarations: [
-    AppComponent,
-    TodoListComponent,
-    TodoDetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
 })
-export class AppModule { }
+export class StudentserviceService {
+  students=[
+            {"Name":"Thota Rajeshwar Rao","Registration":"201900311","Department":"CSE","Semester":5},
+            {"Name":"Roja","Registration":"202000110","Department":"ECE","Semester":3},
+            {"Name":"Ishwarya Rai","Registration":"201800199","Department":"ME","Semester":7},
+            {"Name":"Vyshu","Registration":"201900300","Department":"CSE","Semester":5},
+            {"Name":"Malleshwari","Registration":"202000001","Department":"CE","Semester":1}
+          ]
+  getdetails()
+  {
+    return this.students
+  }
+  constructor() { }
+}
+
