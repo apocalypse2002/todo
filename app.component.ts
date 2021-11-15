b8543f24d254059a19d345b8afef85c27726cf57
apocalypse2002 <80613341+apocalypse2002@users.noreply.github.com>
2021-11-15 16:52:23 +0530

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  spacex:any;
+  link="https://api.spacexdata.com/v3/missions";
+  constructor(private http:HttpClient){
+    this.http.get(this.link).subscribe((spacey:any) =>{
+    this.spacex=spacey;
+    })
+  }
 }
